70254c26f8315489dcb38dd92a09b1b46b315e8b
Matthew Suarez <149050250+matrix505@users.noreply.github.com>
2025-04-17 09:52:29 +0000

    changes in skills.js

--- skills.js
+++ skills.js
@@ -1,10 +1,3 @@
-function calculateNumber(var1,var2){
-    return {
-        addition: var1 + var2,
-        subtraction: var1 - var2,
-        multiplication: var1 * var2,
-        division: var2 !== 0 ? var1 / var2 : 'Division by zero is not allowed'
-    };
+function calculateNumber(var1, var2) {
+    console.log(var1 + var2);
 }
-
-console.log(calculateNumber(10, 5)); // { addition: 15, subtraction: 5, multiplication: 50, division: 2 }
